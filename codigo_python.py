ad23d4f536ca5766905880ebe186c3692c5621fa
elkevo17 <182963@unsaac.edu.pe>
2022-07-06 02:46:59 -0500

    Agregrando matlab

--- codigo_python.py
+++ codigo_python.py
@@ -1,15 +1,4 @@
-print("Hello World")
-print("Hola mundo")
-print("Verficicar esta modicación")
-for i in range(4):
-	print("Ahora si no me pedirá la contraseña")
-
-x = 10
-while 20>x:
-	x = x + 1
-	print(x)
-print("aprendiendo git")
-
-for i in range(5)
-       print(i)
+t=0:0.01:45
+sen=sin(2*pi*60*t)
+plot(t, sen)
 
